2d4b7d53025ae3cb73bba49e4b7c6d4ee42e0f3f
Adam Dąbrowski <contact@adamdabrowski.com>
2016-12-12 03:17:58 +0100

    Fix contact.js v2

--- src/actions/contact.js
+++ src/actions/contact.js
@@ -3,34 +3,31 @@
 import { SubmissionError } from 'redux-form';
 
 import {
-  SUBMIT_MESSAGE_START,
+  /* SUBMIT_MESSAGE_START,
   SUBMIT_MESSAGE_SUCCESS,
   SUBMIT_MESSAGE_FAIL,
-  SUBMIT_MESSAGE_ERROR,
+  SUBMIT_MESSAGE_ERROR,*/
 } from '../constants';
 
-export const submitMessage = (data) => {
-    //dispatch({ type: SUBMIT_MESSAGE_START });
-    return new Promise(async (resolve, reject) => {
-      try {
-        const response = await fetch('https://formspree.io/form@adamdabrowski.com', {
-          headers: { Accept: 'application/json' },
-          method: 'POST',
-          data,
-        });
 
-        if (response.ok) {
-          //dispatch({ type: SUBMIT_MESSAGE_SUCCESS });
-          resolve();
-        } else if (response.status === 400) {
-          throw new SubmissionError(response.statusText);
-        } else {
-          //dispatch({ type: SUBMIT_MESSAGE_FAIL });
-          reject();
-        }
-      } catch (error) {
-        //dispatch({ type: SUBMIT_MESSAGE_ERROR });
-        reject();
-      }
-    });
-}
+export const submit = async data => {
+  try {
+    const response = await fetch(
+      'https://formspree.io/form@adamdabrowski.com',
+      {
+        headers: { Accept: 'application/json' },
+        method: 'POST',
+        data,
+      },
+    );
+    if (response.ok) {
+      return true;
+    } else if (response.status === 400) {
+      throw new SubmissionError(response.statusText);
+    } else {
+      throw response.status;
+    }
+  } catch (error) {
+    throw error;
+  }
+};
