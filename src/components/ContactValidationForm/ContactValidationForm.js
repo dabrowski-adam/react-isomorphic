cf249260fe465b8f6c291bee4f4e65f2e04421f7
Adam Dąbrowski <contact@adamdabrowski.com>
2016-12-13 16:20:30 +0100

    Fix lint errors.

--- src/components/ContactValidationForm/ContactValidationForm.js
+++ src/components/ContactValidationForm/ContactValidationForm.js
@@ -59,7 +59,7 @@ const ContactValidationForm = (props) => {
 };
 
 renderField.propTypes = {
-  // input: PropTypes.array, // ?
+  input: PropTypes.any, // ?
   label: PropTypes.string,
   type: PropTypes.string,
   meta: PropTypes.shape({
