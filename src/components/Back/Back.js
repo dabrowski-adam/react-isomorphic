74ef7b83cf15d25dc89e55081b5085e07d88a429
Adam Dąbrowski <contact@adamdabrowski.com>
2016-12-16 03:24:34 +0100

    Add decorators.

--- src/components/Back/Back.js
+++ src/components/Back/Back.js
@@ -1,17 +1,22 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
 
 import Link from '../Link';
 import ContactValidationForm from '../ContactValidationForm';
 
 import { submitMessage } from '../../actions/contact';
 
+const mapStateToProps = state => ({ isSent: state.contact.isSent });
+
+@connect(mapStateToProps)
 class Back extends Component {
   render() {
+    const { isSent } = this.props;
     return (
       <div className="tile is-vertical" style={{ height: '100%', justifyContent: 'space-between' }}>
         <div className="nav" style={{ marginBottom: '10px' }}>
           <div className="nav-left" />
-          <div className="nav-center"><h2 className="title nav-item">Contact</h2></div>
+          <div className="nav-center"><h2 className="title nav-item">{ isSent ? 'Thank You!' : 'Contact'}</h2></div>
           <div className="nav-right"><Link to="/" className="nav-item"><span className="icon"><i className="fa fa-undo" /></span></Link></div>
         </div>
         <ContactValidationForm onSubmit={submitMessage} />
@@ -19,5 +24,9 @@ class Back extends Component {
     );
   }
 }
+
+Back.propTypes = {
+  isSent: PropTypes.bool.isRequired,
+};
 
 export default Back;
--- src/reducers/contact.js
+++ src/reducers/contact.js
@@ -5,27 +5,24 @@ import {
   SUBMIT_MESSAGE_ERROR,
 } from '../constants';
 
-export default function contact(state = { status: '' }, action) {
+export default function contact(state = { isSent: false }, action) {
   switch (action.type) {
     case SUBMIT_MESSAGE_START:
       return {
         ...state,
-        status: SUBMIT_MESSAGE_START,
       };
     case SUBMIT_MESSAGE_SUCCESS:
       return {
         ...state,
-        status: SUBMIT_MESSAGE_SUCCESS,
+        isSent: true,
       };
     case SUBMIT_MESSAGE_FAIL:
       return {
         ...state,
-        status: SUBMIT_MESSAGE_FAIL,
       };
     case SUBMIT_MESSAGE_ERROR:
       return {
         ...state,
-        status: SUBMIT_MESSAGE_ERROR,
       };
     default:
       return state;
